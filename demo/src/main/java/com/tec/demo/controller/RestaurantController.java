d53c5a4123fb123799f852a02b32e0fae4b2d055
ishansivmal <ishansivmal@gmail.com>
2026-03-06 15:29:02 +0530

    Initialize Spring Boot restaurant management project with Maven setup, entity, repository, service, controller, and Thymeleaf view

--- demo/src/main/java/com/tec/demo/controller/RestaurantController.java
+++ demo/src/main/java/com/tec/demo/controller/RestaurantController.java
@@ -1,10 +1,14 @@
 package com.tec.demo.controller;
 
+import com.tec.demo.model.Restaurant;
 import com.tec.demo.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RestaurantController {
@@ -17,4 +21,30 @@ public class RestaurantController {
         model.addAttribute("listRestaurants", restaurantService.getAllRestaurants());
         return "index";
     }
+
+    //create new restaurant
+    @GetMapping("/new")
+    public String showNewRestaurantForm(Model model) {
+        model.addAttribute("restaurant", new com.tec.demo.model.Restaurant());
+        return "new_restaurant";
+
+    }
+    @PostMapping("/save")
+    public String saveRestaurant(@ModelAttribute("restaurant")Restaurant restaurant) {
+        restaurantService.saveRestaurant(restaurant);
+        return "redirect:/";
+
+    }
+    @GetMapping("/edit/{id}")
+    public String showEditRestaurantForm(@PathVariable Integer id, Model model) {
+        model.addAttribute("restaurant", restaurantService.getRestaurantById(id));
+        return "edit";
+    }
+    @GetMapping("/delete/{id}")
+     public String deleteRestaurant(@PathVariable Integer id) {
+        restaurantService.deleteRestaurant(id);
+        return "redirect:/";
+
+
+     }
 }
